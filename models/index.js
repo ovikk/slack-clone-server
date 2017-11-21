531de3805668ca55631903adbe0e095144bb5f31
ovik <ovikdevil@gmail.com>
2017-11-22 00:40:36 +0200

    added schemas

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize('slack', 'postgres', '123123', {
   dialect: 'postgres',
+  underscored: true,
 });
 
 const models = {
--- models/team.js
+++ models/team.js
@@ -4,12 +4,17 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       unique: true,
     },
-  });
+  },
+  { underscored: true },
+  );
 
   Team.associate = (models) => {
     Team.belongsToMany(models.User, {
       through: 'member',
-      foreignKey: 'teamId',
+      foreignKey: {
+        name: 'teamId',
+        field: 'team_id',
+      },
     });
     Team.belongsTo(models.User, {
       foreignKey: 'owner',
